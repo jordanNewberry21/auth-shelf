4465166343fa4f346c8802e3d4cabf50912ea79b
Brady Baker <bradybaker82@gmail.com>
2021-01-06 14:55:40 -0600

    Oops

--- src/components/AddItemForm/AddItemForm.js
+++ src/components/AddItemForm/AddItemForm.js
@@ -9,12 +9,28 @@ class AddItemForm extends Component {
         }
     }
 
+    handleChangeFor = (event, propertyName) => {
+        this.setState({
+            newItem: {
+                ...this.state.newItem,
+                [propertyName]: event.target.value
+            }
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.dispatch({ type: 'ADD_ITEM', payload: this.state.newItem })
+    }
+
     render() {
         return (
             <div>
                 <p>This is the add item form</p>
-                <form>
-                    <input placeholder="description" onChange={this.handleChangeFor()} />
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="description" onChange={(event) => this.handleChangeFor(event, 'description')} />
+                    <input placeholder="image url" onChange={(event) => this.handleChangeFor(event, 'url')} />
+                    <button type="submit">Add Item</button>
                 </form>
             </div>
         )
