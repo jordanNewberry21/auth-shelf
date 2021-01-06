c2885592a93411d0218f3cd987200ff7228e43e7
carlbarfuss <carlbarfuss@gmail.com>
2021-01-06 16:10:41 -0600

    added delete saga

---
+++ src/redux/sagas/deleteShelf.saga.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* deleteShelf(action) {
+   try {
+      yield axios.delete(`/api/shelf/${action.payload.itemId}`)
+      yield put({ type: 'FETCH_SHELF' })
+   } catch (error) {
+      console.log('Shelf DELETE request failed', error);
+   }
+}
+
+function* deleteShelfSaga() {
+   yield takeLatest('DELETE_ITEM', deleteShelf);
+}
+
+
+export default deleteShelfSaga;
